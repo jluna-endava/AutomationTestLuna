c50555cee787efeca625bc5efb912f505768b48a
Juan Pablo Luna Mejia <Juan-Pablo.Luna@endava.com>
2023-06-01 14:37:42 -0500

    First Commit

---
+++ cypress/e2e/init.cy.js
@@ -0,0 +1,8 @@
+describe('Test Set Blaze Demo', () => {
+  it('enter demoblaze', () => {
+    cy.visit('')
+  })
+  it('check product in demoblaze',()=>{
+    cy.visit('/prod.html?idp_=1')
+  })
+})
